e6b7dc3bfbefd648ccf273583726f8eb36019a84
Rapzzcode <rapzzcode@gmail.com>
2025-08-09 16:15:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  botToken: "18", // Ganti dengan token dari @BotFather
+  ownerId: "123456789",      // ID Telegram owner
+  ownerName: "Isi dengan nama kamu", // Nama owner
+  ownerUsername: "rapzzgege", // Nama telegram owner
+  botName: "NamaBot"    // Nama bot
+};
